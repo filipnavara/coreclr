2cba5eaff455ba8349d03d5525a396e4e2e54f24
Sergey Yanchi <sergeyyanchi@unity3d.com>
2017-05-22 11:31:57 +0200

    added env variable UNITY_MONO_ROOT instead of fiull path

--- unity/simple-app/build_and_run.sh
+++ unity/simple-app/build_and_run.sh
@@ -1,2 +1,2 @@
-gcc main.cpp -I/Users/sergeyyanchi/development/hackweek2017/Unity/MonoBleedingEdge/include/mono -I/Users/sergeyyanchi/development/hackweek2017/Unity -DENABLE_MONO -DPLATFORM_STANDALONE
+gcc main.cpp -I$UNITY_MONO_ROOT/MonoBleedingEdge/include/mono -I$UNITY_MONO_ROOT -DENABLE_MONO -DPLATFORM_STANDALONE
 ./a.out
