70b113032dd059b7ee23d361b3c3827073b28447
Alexandre Mutel <alexandre_mutel@live.com>
2017-05-24 09:57:50 +0200

    Allow the MonoProxyCodeGen to output empty functions and the list of exports for a specific platform

--- unity/mono-proxy/MonoProxyCodeGen/Program.cs
+++ unity/mono-proxy/MonoProxyCodeGen/Program.cs
@@ -22,11 +22,17 @@ namespace MonoProxyCodeGen
         static void Main(string[] args)
         {
             //var writer = Console.Out;
-            var writer = new StreamWriter(Path.Combine(Environment.CurrentDirectory,
-                "../../../mono-proxy/monoproxy.cpp"));
+
+            // Defines the platform
+            //var platform = "PLATFORM_OSX";
+            var platform = "PLATFORM_WIN";
+            const bool generateProxy = false;
+            var fileOutput = generateProxy ? "../../../mono-proxy/monoproxy.cpp" : "monoproxy_decl.cpp";
+
+            var writer = new StreamWriter(Path.Combine(Environment.CurrentDirectory, fileOutput));
 
             var processStartInfo = new ProcessStartInfo(@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Tools\MSVC\14.10.25017\bin\HostX86\x86\cl.exe",
-                $"/E /nologo /I{Environment.GetEnvironmentVariable("UNITY_ROOT")}\\runtime\\mono MonoFunctionsProxy.h");
+                $"/E /nologo /D{platform}=1 /I{Environment.GetEnvironmentVariable("UNITY_ROOT")}\\runtime\\mono MonoFunctionsProxy.h");
 
             processStartInfo.UseShellExecute = false;
             processStartInfo.RedirectStandardOutput = true;
@@ -40,12 +46,16 @@ namespace MonoProxyCodeGen
             var split = new char[] {'|'};
             var splitParams = new char[] {','};
 
-            writer.WriteLine("// This file is generated automatically DO NOT EDIT");
-            writer.WriteLine("// Generated with MonoProxyCodeGen");
-            writer.WriteLine();
-            writer.WriteLine("#include \"monoproxy.h\"");
+            if (generateProxy)
+            {
+                writer.WriteLine("// This file is generated automatically DO NOT EDIT");
+                writer.WriteLine("// Generated with MonoProxyCodeGen");
+                writer.WriteLine();
+                writer.WriteLine("#include \"monoproxy.h\"");
+            }
 
             var regexArgName = new Regex(@"[A-Za-z_][A-Za-z_0-9]*(\[\])?$");
+            var functions = new List<string>();
             while ((line = reader.ReadLine()) != null)
             {
                 if (!line.StartsWith("|"))
@@ -78,19 +88,45 @@ namespace MonoProxyCodeGen
                     }
                     paramRedirect.Append(param.Name);
                 }
-
+                
                 var callback = $"__{functionName}__";
                 writer.WriteLine();
-                writer.WriteLine($"typedef {returnType} (*type_{functionName}){entries[2]};");
-                writer.WriteLine($"type_{functionName} {callback};");
-                writer.WriteLine($"extern \"C\" EXPORT_API {returnType} {functionName}{entries[2]}");
+
+                if (generateProxy)
+                {
+                    writer.WriteLine($"typedef {returnType} (*type_{functionName}){entries[2]};");
+                    writer.WriteLine($"type_{functionName} {callback};");
+                }
+                var export = generateProxy ? "EXPORT_API " : "";
+                writer.WriteLine($"extern \"C\" {export}{returnType} {functionName}{entries[2]}");
                 writer.WriteLine("{");
-                writer.WriteLine($"    log_function_usage(\"{functionName}\");");
-                writer.WriteLine($"    if (!{callback})");
-                writer.WriteLine($"        {callback} = (type_{functionName})load_mono_function(\"{functionName}\");");
-                writer.Write(returnType == "void" ? "    " : "    return ");
-                writer.WriteLine($"{callback}({paramRedirect});");
+                if (generateProxy)
+                {
+                    writer.WriteLine($"    log_function_usage(\"{functionName}\");");
+                    writer.WriteLine($"    if (!{callback})");
+                    writer.WriteLine(
+                        $"        {callback} = (type_{functionName})load_mono_function(\"{functionName}\");");
+                    writer.Write(returnType == "void" ? "    " : "    return ");
+                    writer.WriteLine($"{callback}({paramRedirect});");
+                }
+                else
+                {
+                    writer.WriteLine($"    ASSERT_NOT_IMPLEMENTED;");
+                    if (returnType != "void")
+                    {
+                        writer.WriteLine($"    return ({returnType})0;");
+                    }
+                }
                 writer.WriteLine("}");
+
+                functions.Add(functionName);
+            }
+
+            writer.WriteLine();
+            writer.WriteLine($"// Exported Functions for platform {platform}");
+            foreach (var function in functions)
+            {
+                writer.WriteLine($"// {function}");
             }
 
             writer.Flush();
